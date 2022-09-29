2246028e24319fd48d5faaca895c1b73f62e7638
Sam Avery <sam.avery1@gmail.com>
2022-09-29 16:58:34 -0500

    commit tree 9b946f63fbb8bbe2844f6c960b2d2f231850397a

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const actorList = actors.map((actor) => {
+    return (
+      <div key={actor.name}>
+        <p>Name: {actor.name}</p>
+        <ul>Movies:
+          {actor.movies.map((movies) => {
+            return (
+              <li key={movies}>{movies}</li>
+            )
+          })}
+        </ul>
+      </div>
+    ) })
+  
+  return <div>
+      <h1>Actors Page</h1>
+      {actorList}
+    </div>;
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const directorList = directors.map((director) => {
+    return (
+      <div key={director.name}>
+        <p>Name: {director.name}</p>
+        <ul>Movies:
+          {director.movies.map((movies) => {
+            return (
+              <li key={movies}>{movies}</li>
+            )
+          })}
+        </ul>
+      </div>
+    ) })
+  
+
+
+  return <div>
+    <h1>Directors Page</h1>
+    {directorList}
+  </div>;
 }
 
 export default Directors;
